0daab0eed85bde6489b8790bcb81fd26debbe840
aastha <aasthabarve0@gmail.com>
2023-04-10 20:13:54 +0530

    added: add master

---
+++ src/com/bridgelabz/employeewageday8/EmployeeWageProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewageday8;
+
+public class EmployeeWageProblem {
+    public static void main(String args[]){
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+
+}
